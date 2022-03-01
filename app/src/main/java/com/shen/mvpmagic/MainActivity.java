04cbc2356f45e86c83feb7f8e2ca441f2f041c87
Shenhe <mnbasdzxc@721108>
2022-03-01 18:55:39 +0800

    1

---
+++ app/src/main/java/com/shen/mvpmagic/MainActivity.java
@@ -0,0 +1,19 @@
+package com.shen.mvpmagic;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author:Shen
+ * @time: 2022/3/1 18:02
+ * @desc:
+ **/
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
